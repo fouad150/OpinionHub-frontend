810845dfa3421a2adce08aca3d6581d01094a619
fouad <fouadwork2@gmail.com>
2024-03-09 16:54:55 +0100

    add image to publication , and add show more text option

--- src/app/component/publication/publication-list/publication-list.component.ts
+++ src/app/component/publication/publication-list/publication-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef} from '@angular/core';
+import {Component, ElementRef, OnInit,} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 
 @Component({
@@ -6,10 +6,11 @@ import {ActivatedRoute} from "@angular/router";
   templateUrl: './publication-list.component.html',
   styleUrls: ['./publication-list.component.css'],
 })
-export class PublicationListComponent {
+
+export class PublicationListComponent implements OnInit{
   icon: any = {};
   iconChecked:boolean=false;
-  constructor(private elementRef: ElementRef ) {
+  constructor(private elementRef: ElementRef) {
   }
 
   changeLikeIconColor(element: HTMLElement) {
@@ -24,4 +25,32 @@ export class PublicationListComponent {
 
   }
 
+  isTruncated: boolean = true;
+  truncatedText: string = '';
+  fullText: string = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo recusandae nulla rem eos ipsa praesentium esse magnam nemo dolor sequi fuga quia quaerat cum, obcaecati hic, molestias minima iste voluptates.`;
+  buttonText: string = '...more';
+
+  ngOnInit(): void {
+    this.truncateText();
+  }
+
+  toggleTruncate(): void {
+    this.isTruncated = !this.isTruncated;
+    if (this.isTruncated) {
+      this.truncateText();
+      this.buttonText = '...more';
+    } else {
+      this.truncatedText = this.fullText;
+      this.buttonText = 'show less';
+    }
+  }
+
+  truncateText(): void {
+    const words = this.fullText.split(' ');
+    if (words.length > 15) {
+      this.truncatedText = words.slice(0, 15).join(' ') /*+ '...'*/;
+    } else {
+      this.truncatedText = this.fullText;
+    }
+  }
 }
